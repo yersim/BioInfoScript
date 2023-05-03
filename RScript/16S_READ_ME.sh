4031b1815794895c2c7b4e587ce33d742f9f0950
Simon Yersin <108127320+yersim@users.noreply.github.com>
2023-05-03 17:37:02 +0200

    Add files via upload

---
+++ RScript/16S_READ_ME.sh
@@ -0,0 +1,21 @@
+READ ME file for the template of 16S rRNA amplicon sequencing data
+
+1.
+Create a folder with the raw data (fastq files)
+Save DADA2_PairedReads.Rmd file in the folder
+
+2. 
+Start with the DADA2 pipeline
+Once completed, be sure to have the correct file saved in the folder
+
+3. 
+Use 16S_table_to_phyloseq to obtain the code to start your analysis
+This code can be copy and paste in your analysis R script
+At the end of this pipeline you will have a phyloseq object that can be used for analysis
+
+4. 
+Analysis!
+Example of analysis and processing before analysis can be found in the 16S_analysis.Rmd
+The code can be copy and paste below the 16S_table_to_phyloseq code in your R script
+
+Good luck!
